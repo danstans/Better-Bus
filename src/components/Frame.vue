2f0449e69888c2b6b93cf6a86e5818e6d937a430
Daniel Stansberry <danielsstansberry@gmail.com>
2018-01-31 10:09:31 -0800

    moved frame to its own file

---
+++ src/components/Frame.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="frame">
+    <div class="frame-left">
+      BetterBus
+    </div>
+    <div class="frame-right">
+      Login
+    </div>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'Frame'
+  }
+</script>
+
+<style lang="scss" scoped>
+.frame {
+  height: 56px;
+  width: 100vw;
+  background: #35495E;
+  color: #fff;
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+
+  >div {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    width: 30%;
+  }
+}
+</style>
