64dca6c86f2569597947758038b3eded373633fa
Kyle Salter <kyle.salter@aero.org>
2018-03-05 12:34:01 -0800

    bus data script

---
+++ db_scripts/createDb.js
@@ -0,0 +1,25 @@
+const _ = require('lodash')
+const moment = require('moment')
+const fs = require('fs')
+
+const busesStartTimes = require('./startTimes.json')
+const busStops = require('./busStops.json')
+const timeIntervals = require('./timeIntervals.json')
+
+const busData = {}
+
+_.each(busesStartTimes, (busStartTimes, busRoute) => {
+  _.each(busStartTimes, (startTime) => {
+    let startTimeUCT = moment(startTime, 'hh:mmA')
+    for (let i in busStops) {
+      let busStop = busStops[i]
+      let interval = timeIntervals[i]
+      let busTime = {
+        busRoute: busRoute,
+        time: startTimeUCT.add(interval, 'minutes').format('hh:mmA')
+      }
+      busData[busStop] ? busData[busStop].push(busTime) : (busData[busStop] = []).push(busTime)
+    }
+  })
+})
+fs.writeFileSync('./data.json', JSON.stringify(busData, null, 4), 'utf-8')
