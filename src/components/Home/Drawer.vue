7ba0cb4d0396960cc5363bc13ce39bfd611b469c
TiffanyLee <tiffanylee751@gmail.com>
2018-03-07 14:39:03 -0800

    Added close button and login authentication

--- src/components/Home/Drawer.vue
+++ src/components/Home/Drawer.vue
@@ -9,14 +9,25 @@
       </div>
     </div>
     <div class="drawer-large" v-else-if="showLogin">
-      <div class="login-form">
-        This is the Login
-      </div>
+      <form class="login-form" @submit.prevent="loginUser">
+        <div class="exit-button" @click="clickClose">
+          <button type="submit">Close</button>
+        </div>
+        <span>Log In</span>        
+        <input v-model="login.email" type="text" placeholder="Email">
+        <input v-model="login.password" type="password" placeholder="Password">
+        <div class="login-button">
+          <button type="submit">Login</button>
+        </div>
+      </form>
     </div>
     <div class="drawer-large" v-else-if="showSignup">
       <form class="signup-form" @submit.prevent="signUpUser">
+        <div class="exit-button" @click="clickClose">
+          <button type="submit">Close</button>
+        </div>
         <span>Sign Up</span>        
-        <input v-model="signup.username" type="text" placeholder="Username">
+        <input v-model="signup.email" type="text" placeholder="email">
         <input v-model="signup.password" type="password" placeholder="Password">
         <div class="signup-button">
           <button type="submit">Sign Up</button>
@@ -36,14 +47,19 @@
         showLogin: false,
         showSignup: false,
         signup: {
-          username: '',
+          email: '',
+          password: ''
+        },
+        login: {
+          email: '',
           password: ''
         }
       }
     },
     methods: {
       ...mapActions([
-        'signUpFirebase'
+        'signUpFirebase',
+        'loginUserFirebase'
       ]),
       clickLogin: function () {
         this.showUnauth = false
@@ -53,12 +69,25 @@
         this.showUnauth = false
         this.showSignup = true
       },
+      clickClose: function () {
+        this.showUnauth = true
+        this.showSignup = false
+        this.showLogin = false
+      },
       signUpUser: function () {
-        if (!this.signup.password || !this.signup.username) {
+        if (!this.signup.password || !this.signup.email) {
           console.log('one of these fields are incomplete')
         } else {
           // Create user account and store into state
-          this.signUpFirebase({'username': this.signup.username, 'password': this.signup.password})
+          this.signUpFirebase({'email': this.signup.email, 'password': this.signup.password})
+        }
+      },
+
+      loginUser: function () {
+        if (!this.login.password || !this.login.email) {
+          console.log('one of these fields are incomplete')
+        } else {
+          this.loginUserFirebase({'email': this.login.email, 'password': this.login.password})
         }
       }
     }
@@ -106,9 +135,14 @@
     height: 80%;
     width: 80%;
     display: grid;
-    grid-template-rows: 25% 40px 40px 1fr; 
+    grid-template-rows: 5px 25% 40px 40px 1fr; 
     grid-row-gap: 16px;
 
+    .exit-button {
+      margin-top: -10px;
+      padding-left: 450px;
+
+    }
     span {
       display: flex;
       justify-content: center;
@@ -126,5 +160,37 @@
       }
     }
   }
+
+  .login-form {
+    box-sizing: border-box;
+    height: 80%;
+    width: 80%;
+    display: grid;
+    grid-template-rows: 5px 25% 40px 40px 1fr; 
+    grid-row-gap: 16px;
+
+    .exit-button {
+      margin-top: -10px;
+      padding-left: 450px;
+
+    }
+    span {
+      display: flex;
+      justify-content: center;
+      align-items: center;
+      font-size: 24px;  
+    }
+
+    .login-button {
+      position: relative;
+
+      button {
+        position: absolute;
+        right: 15px;
+        top: 15px;
+      }
+    }
+  }
+
 }
 </style>
--- src/store/actions.js
+++ src/store/actions.js
@@ -5,9 +5,29 @@ export default {
     console.log('you aer here in sign up')
     console.log(`the value of the payload is ${payload}`)
     console.log(`The value of username is ${payload.username}`)
-    firebase.auth().createUserWithEmailAndPassword(payload.username, payload.password).then(response => {
+    firebase.auth().createUserWithEmailAndPassword(payload.email, payload.password).then(response => {
       console.log('user signed in')
       commit('SET_USER_STATE', true)
+    }).catch(err => {
+      console.log(err)
+    })
+  },
+
+  loginUserFirebase: function ({commit}, payload) {
+    console.log(`The value of username is ${payload.email}`)
+    firebase.auth().signInWithEmailAndPassword(payload.email, payload.password).then(response => {
+      console.log('user logged in')
+      commit('SET_USER_STATE', true)
+    }).catch(err => {
+      console.log(err)
+    })
+  },
+
+  logoutUserFirebase: function ({commit}) {
+    firebase.auth().signOut().then(response => {
+      // Sign-out successful.
+      console.log('user signed out')
+      commit('SET_USER_STATE', false)
     }).catch(err => {
       console.log(err)
     })
